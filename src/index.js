bcd4e394b3e44978d7ecff722d8acf991fc2104e
AndresJejen <gajcam@gmail.com>
2019-01-30 16:38:38 -0500

    cambios demo view sin terminar

--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ const path = require('path')            //Importa la libreria de rutas
 const config = require('./config')      //Importa el modulo de configuraciones globales
 const mongoose = require('mongoose');
 
-
-
-
 //Conexion Base de datos y lanzamiento del servidor
 mongoose.connect(`mongodb://${config.MONGODB_USER}:${config.MONGODB_PASSWORD}@${config.MONGODB_CONNSTR}:${process.env.MONGODB_PORT}/${config.MONGODB_DB}`,
 {
@@ -23,27 +20,4 @@ mongoose.connect(`mongodb://${config.MONGODB_USER}:${config.MONGODB_PASSWORD}@${
     })
   .catch((err) => {
     console.error(err);
-  });
-
-  /*
-  mongoose.connect(process.env.COSMOSDB_CONNSTR, {
-    useNewUrlParser: true
-    ,auth: {
-      user: process.env.COSMODDB_USER,
-      password: process.env.COSMOSDB_PASSWORD
-    }
-  })
-  .then(() => {
-        console.log('Connection to MLab MongoDb successful OK')
-        //Ejecución del servidor
-        app.listen(config.port,(err)=>{
-            console.log(`Servidor ejecutandose en http://${app.address().address}:${config.port}`)
-        });        
-    })
-  .catch((err) => {
-    console.error(err);
-    console.log(process.env.COSMOSDB_CONNSTR)
-
-  });
-  */
-
+});
