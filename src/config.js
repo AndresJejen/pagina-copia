161bff1c28700d208124b593e69f4acfd9d587f9
AndresJejen <gajcam@gmail.com>
2019-01-30 14:59:17 -0500

    Base de datos Configurada

--- src/config.js
+++ src/config.js
@@ -1,3 +1,10 @@
+require('dotenv').config()
+
 module.exports = {
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+    MONGODB_CONNSTR : process.env.MONGODB_CONNSTR,
+    MONGODB_DB : process.env.MONGODB_DB,
+    MONGODB_PORT : process.env.MONGODB_PORT,
+    MONGODB_USER : process.env.MONGODB_USER,
+    MONGODB_PASSWORD :process.env.MONGODB_PASSWORD
 }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,46 @@
 const app = require('./app')            //Importa el servidor app
 const path = require('path')            //Importa la libreria de rutas
 const config = require('./config')      //Importa el modulo de configuraciones globales
+const mongoose = require('mongoose');
 
 
-//Lanzamiento del servidor
-app.listen(config.port,(err)=>{
-    console.log(`Servidor ejecutandose en http://${app.address().address}:${config.port}`)
+
+
+//Conexion Base de datos y lanzamiento del servidor
+mongoose.connect(`mongodb://${config.MONGODB_USER}:${config.MONGODB_PASSWORD}@${config.MONGODB_CONNSTR}:${process.env.MONGODB_PORT}/${config.MONGODB_DB}`,
+{
+  useNewUrlParser:true
 })
+  .then(() => {
+        console.log('Connection to MLab MongoDb successful OK')
+        //Ejecución del servidor
+        app.listen(config.port,(err)=>{
+            console.log(`Servidor ejecutandose en http://${app.address().address}:${config.port}`)
+        });        
+    })
+  .catch((err) => {
+    console.error(err);
+  });
+
+  /*
+  mongoose.connect(process.env.COSMOSDB_CONNSTR, {
+    useNewUrlParser: true
+    ,auth: {
+      user: process.env.COSMODDB_USER,
+      password: process.env.COSMOSDB_PASSWORD
+    }
+  })
+  .then(() => {
+        console.log('Connection to MLab MongoDb successful OK')
+        //Ejecución del servidor
+        app.listen(config.port,(err)=>{
+            console.log(`Servidor ejecutandose en http://${app.address().address}:${config.port}`)
+        });        
+    })
+  .catch((err) => {
+    console.error(err);
+    console.log(process.env.COSMOSDB_CONNSTR)
+
+  });
+  */
+
